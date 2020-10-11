9bb69e0748db9183383372eab99d464bc2ef07fc
Jijo Bose <bosejijo@gmail.com>
2020-10-11 14:09:31 +0530

    mining difficulty

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
   let data, lastBlock, block;
@@ -7,12 +8,18 @@ describe('Block', () => {
     data = 'bar';
     lastBlock = Block.genesis();
     block = Block.mineBlock(lastBlock, data);
-  })
+  });
+
   it('sets the `data` to match the input', () => {
     expect(block.data).toEqual(data);
   });
 
   it('sets the `lastHash` to match the hash of the last block', () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it('generates a hash that mathches the difficulty', () => {
+    expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+    console.log(block.toString());
   });
 });
