e0a58dd93319b4186996961c7c997512fb0e54c2
Jijo Bose <bosejijo@gmail.com>
2020-10-11 11:22:49 +0530

    message handler

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,6 +3,8 @@ const Websocket = require('ws');
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
+// HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+
 
 class P2pServer {
   constructor(blockchain) {
@@ -29,6 +31,17 @@ class P2pServer {
   connectSocket(socket) {
     this.sockets.push(socket);
     console.log('Socket connected');
+
+    this.messageHandler(socket);
+
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+    });
   }
 }
 
